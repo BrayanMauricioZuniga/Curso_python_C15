c2f2a4148a21f2f5e1b9c17ff572b86d769122d0
BrayanMauricioZuniga <brayanmzg9503@gmail.com>
2023-10-27 18:29:32 -0500

    Prueba 1 try except

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -4,9 +4,15 @@ ejercicioActivo = True
 
 
 while (ejercicioActivo):
-    numero = int(input(f"Digite un número: "))    
-    if  numero%2 == 0:
-        print(f"El número ingresado es par")
-    else:
-        print(f"El numero ingresado es impar")
- 
+    numero = int(input(f"Digite un número: "))
+    try:    
+        if numero != 100:
+            if  numero%2 == 0:
+                print(f"El número ingresado es par")
+            else:
+                print(f"El número ingresado es impar")
+        else:
+            print("Saliendo del ejercicio...")
+            ejercicioActivo = False
+    except ValueError:
+        print("`numero` Debes ser un número entero")
