1ebf5634b02fe3e03a3882dfb6fd87b371013c78
BrayanMauricioZuniga <brayanmzg9503@gmail.com>
2023-10-27 18:02:29 -0500

    Ejercicio número par

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,2 +1,12 @@
-# Ejercicio 1
+# Ejercicio. Numero par
 
+ejercicioActivo = True
+
+
+while (ejercicioActivo):
+    numero = int(input(f"Digite un número: "))    
+    if  numero%2 == 0:
+        print(f"El número ingresado es par")
+    else:
+        print(f"El numero ingresado es impar")
+ 
